cdd644012c8e0be08a64ad4d5881a723d02ae4aa
ricardo rodrigues <ricardo.rodrigues@banetech.net>
2025-07-19 18:31:07 +0100

    tt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Yoo
+          Learn React rfROD
         </a>
       </header>
     </div>
